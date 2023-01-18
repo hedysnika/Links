0f01a40867d7d9b979c38022640db926e930d149
Nikkolla <arebanikko@gmail.com>
2023-01-18 11:33:32 +0400

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ function App() {
     <div className='h-auto min-h-screen'>
       <div className='flex items-center justify-center h-screen text-center'>
      <pre><div className='text-3xl font-semibold'>τὰ φίλτατα Nike</div> <br />
-      <div className='flex justify-center'>Github <Icon icon="mdi:github" color="black" width="24" height="24" /></div>
-      <div className='flex justify-center'>Linkedin<Icon icon="mdi:linkedin" color="black" width="24" height="24" /></div>
-      <div className='flex justify-center'>twitter<Icon icon="ph:twitter-logo" color="black" width="24" height="24" /></div>
-      <div className='flex justify-center'>Portfolio Website</div>
+      <div className='flex justify-center mt-4 mb-3'>Github <Icon icon="mdi:github" color="black" width="24" height="24" /></div>
+      <div className='flex justify-center my-3'>Linkedin<Icon icon="mdi:linkedin" color="black" width="24" height="24" /></div>
+      <div className='flex justify-center my-3'>twitter<Icon icon="ph:twitter-logo" color="black" width="24" height="24" /></div>
+      <div className='flex justify-center my-3'>Portfolio Website</div>
       </pre> 
       </div>
     </div>
